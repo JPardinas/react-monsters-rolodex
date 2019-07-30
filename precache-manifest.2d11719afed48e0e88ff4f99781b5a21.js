1fb8e8cd655c8dd0378db111570e648b9afdaec8
Juan Pardiñas Bárbara <jpardinas23@gmail.com>
2019-07-30 20:33:18 +0200

    Updates

---
+++ precache-manifest.2d11719afed48e0e88ff4f99781b5a21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84802d7be40acc02f461a40f10b00bf1",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4048daba790622cf315f",
+    "url": "/react-monsters-rolodex/static/css/main.16ceca93.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/react-monsters-rolodex/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "4048daba790622cf315f",
+    "url": "/react-monsters-rolodex/static/js/main.b23bd28e.chunk.js"
+  },
+  {
+    "revision": "09fc101723e82bb4a8f4",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.2a38a34c.js"
+  }
+]);
